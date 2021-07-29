f3c4a16dd33bb8b6423215f1640dcb3a977fef64
tungbach1990 <bachntb2vn@gmail.com>
2021-07-29 22:42:34 +0700

    correct position EF

--- src/map/script_constants.hpp
+++ src/map/script_constants.hpp
@@ -3797,10 +3797,10 @@
 	export_constant(RC2_HEARTHUNTER);
 	export_constant(RC2_ROCKRIDGE);
 	export_constant(RC2_WERNER_LAB);
-	export_constant(RC2_EP172BETA);
-	export_constant(RC2_EP172ALPHA);	
 	export_constant(RC2_TEMPLE_DEMON);
 	export_constant(RC2_ILLUSION_VAMPIRE);
+	export_constant(RC2_EP172BETA);
+	export_constant(RC2_EP172ALPHA);		
 	export_constant(RC2_MAX);
 
 	/* monster ai */
